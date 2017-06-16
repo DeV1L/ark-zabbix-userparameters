0f0c1ac5041c33455f1a192ba1ffa2253b7baa00
y.savchenko <y.savchenko@arkadium.com>
2017-06-16 14:36:54 +0300

    delete spare

--- Zabbix_IIS8_monitoring/get_apppoolpid.ps1
+++
@@ -1,4 +0,0 @@
-param ([string] $name = 0)
-Import-Module WebAdministration
-$AppPoolPID = (gwmi -NS 'root\WebAdministration' -class 'WorkerProcess' | Where-Object -Property AppPoolName -eq "$name").ProcessId
-Write-Output ($AppPoolPID)
